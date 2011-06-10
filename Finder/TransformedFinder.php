b6c4a582d3249d47a6166daba7ca7f84f1206c0b
ornicar <thibault.duplessis@gmail.com>
2011-06-10 16:03:17 -0700

    Fix configuration and indentation

--- Finder/TransformedFinder.php
+++ Finder/TransformedFinder.php
@@ -27,16 +27,16 @@ class TransformedFinder implements FinderInterface, PaginatedFinderInterface
     /**
      * Search for a query string
      *
-	 * @param string $query
-	 * @param int $limit
+     * @param string $query
+     * @param integer $limit
      * @return array of model objects
      **/
     public function find($query, $limit = null)
     {
-		$queryObject = Elastica_Query::create($query);
-		if (null !== $limit) {
-			$queryObject->setLimit($limit);
-		}
+        $queryObject = Elastica_Query::create($query);
+        if (null !== $limit) {
+            $queryObject->setLimit($limit);
+        }
         $results = $this->searchable->search($queryObject)->getResults();
 
         return $this->transformer->transform($results);
@@ -49,11 +49,11 @@ class TransformedFinder implements FinderInterface, PaginatedFinderInterface
      **/
     public function findPaginated($query)
     {
-		$queryObject = Elastica_Query::create($query);
+        $queryObject = Elastica_Query::create($query);
         $results = $this->searchable->search($queryObject)->getResults();
-		$paginatorAdapter = $this->createPaginatorAdapter($queryObject);
+        $paginatorAdapter = $this->createPaginatorAdapter($queryObject);
 
-		return new Paginator($paginatorAdapter);
+        return new Paginator($paginatorAdapter);
     }
 
     /**
@@ -64,6 +64,6 @@ class TransformedFinder implements FinderInterface, PaginatedFinderInterface
      */
     protected function createPaginatorAdapter(Elastica_Query $query)
     {
-		return new TransformedPaginatorAdapter($this->searchable, $query, $this->transformer);
+        return new TransformedPaginatorAdapter($this->searchable, $query, $this->transformer);
     }
 }
