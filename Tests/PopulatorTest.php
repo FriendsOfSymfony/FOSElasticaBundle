d0462716905c5d1def134e1f2b25346134770328
Leszek <leszek.prabucki@gmail.com>
2011-07-07 22:41:46 +0200

    Added Populator test

---
+++ Tests/PopulatorTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace FOQ\ElasticaBundle\Tests\Populator;
+
+use FOQ\ElasticaBundle\Populator;
+use FOQ\ElasticaBundle\Provider\ProviderInterface;
+use Closure;
+
+class PopulatorMock extends Populator
+{
+    public $providers = array();
+}
+
+class PopulatorTest extends \PHPUnit_Framework_TestCase
+{
+  public function testThatWeCanAddProvider()
+  {
+    $provider = $this->getMock('FOQ\ElasticaBundle\Provider\ProviderInterface', array('populate'));
+    
+    $populator = new PopulatorMock(array());
+    $populator->addProvider('l3l0Provider', $provider);
+   
+    $this->assertEquals(count($populator->providers), 1);
+    $this->assertArrayHasKey('l3l0Provider', $populator->providers);
+    $this->assertInstanceOf('FOQ\ElasticaBundle\Provider\ProviderInterface', $populator->providers['l3l0Provider']);
+  }
+
+  public function testThatPopulateThroughProviders()
+  {
+    $provider = $this->getMock('FOQ\ElasticaBundle\Provider\ProviderInterface', array('populate'));
+    $provider->expects($this->once())
+      ->method('populate');
+
+    $populator = new Populator(array('l3l0Provider' => $provider));
+    $populator->populate(function ($text) { return $text; });
+  }
+}
