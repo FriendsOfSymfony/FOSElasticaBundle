f627645481108f38f55f1cee559e59cc477438d3
Leszek <leszek.prabucki@gmail.com>
2011-07-07 22:58:43 +0200

    Fixed indentations

--- Tests/PopulatorTest.php
+++ Tests/PopulatorTest.php
@@ -13,25 +13,25 @@ class PopulatorMock extends Populator
 
 class PopulatorTest extends \PHPUnit_Framework_TestCase
 {
-  public function testThatWeCanAddProvider()
-  {
-    $provider = $this->getMock('FOQ\ElasticaBundle\Provider\ProviderInterface', array('populate'));
-    
-    $populator = new PopulatorMock(array());
-    $populator->addProvider('l3l0Provider', $provider);
-   
-    $this->assertEquals(count($populator->providers), 1);
-    $this->assertArrayHasKey('l3l0Provider', $populator->providers);
-    $this->assertInstanceOf('FOQ\ElasticaBundle\Provider\ProviderInterface', $populator->providers['l3l0Provider']);
-  }
+    public function testThatWeCanAddProvider()
+    {
+        $provider = $this->getMock('FOQ\ElasticaBundle\Provider\ProviderInterface', array('populate'));
+  
+        $populator = new PopulatorMock(array());
+        $populator->addProvider('l3l0Provider', $provider);
 
-  public function testThatPopulateThroughProviders()
-  {
-    $provider = $this->getMock('FOQ\ElasticaBundle\Provider\ProviderInterface', array('populate'));
-    $provider->expects($this->once())
-      ->method('populate');
-
-    $populator = new Populator(array('l3l0Provider' => $provider));
-    $populator->populate(function ($text) { return $text; });
-  }
+        $this->assertEquals(count($populator->providers), 1);
+        $this->assertArrayHasKey('l3l0Provider', $populator->providers);
+        $this->assertInstanceOf('FOQ\ElasticaBundle\Provider\ProviderInterface', $populator->providers['l3l0Provider']);
+    }
+  
+    public function testThatPopulateThroughProviders()
+    {
+        $provider = $this->getMock('FOQ\ElasticaBundle\Provider\ProviderInterface', array('populate'));
+        $provider->expects($this->once())
+          ->method('populate');
+  
+        $populator = new Populator(array('l3l0Provider' => $provider));
+        $populator->populate(function ($text) { return $text; });
+    }
 }
--- Tests/ReseterTest.php
+++ Tests/ReseterTest.php
@@ -45,65 +45,65 @@ class ReseterTest extends \PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-      if (!class_exists('Elastica_Exception_Response') || !class_exists('Elastica_Response'))
-      {
-        $this->markTestSkipped('The Elastica library classes are not available');
-      }
+        if (!class_exists('Elastica_Exception_Response') || !class_exists('Elastica_Response'))
+        {
+          $this->markTestSkipped('The Elastica library classes are not available');
+        }
     }
 
     public function testThatResetMethodDeleteAllIndexes()
     {
-       $indexManager = new IndexManager(array(
-           'index_1' => new Index(),
-           'index_2' => new Index()
-       ), new Index());
-       
-       $reseter = new Reseter($indexManager);
-       $reseter->reset();
+        $indexManager = new IndexManager(array(
+            'index_1' => new Index(),
+            'index_2' => new Index()
+        ), new Index());
+        
+        $reseter = new Reseter($indexManager);
+        $reseter->reset();
 
-       $this->assertTrue($indexManager->getIndex('index_1')->deleted);
-       $this->assertTrue($indexManager->getIndex('index_2')->deleted);
+        $this->assertTrue($indexManager->getIndex('index_1')->deleted);
+        $this->assertTrue($indexManager->getIndex('index_2')->deleted);
     }
 
     public function testThatResetMethodDoesNotDeleteNewIndexes()
     {
-       $indexManager = new IndexManager(array(
-           'index_1' => new Index(),
-           'index_2' => new NewIndex()
-       ), new Index());
-       
-       $reseter = new Reseter($indexManager);
-       $reseter->reset();
+        $indexManager = new IndexManager(array(
+            'index_1' => new Index(),
+            'index_2' => new NewIndex()
+        ), new Index());
+        
+        $reseter = new Reseter($indexManager);
+        $reseter->reset();
 
-       $this->assertTrue($indexManager->getIndex('index_1')->deleted);
-       $this->assertFalse($indexManager->getIndex('index_2')->deleted);
+        $this->assertTrue($indexManager->getIndex('index_1')->deleted);
+        $this->assertFalse($indexManager->getIndex('index_2')->deleted);
     }
     
     public function testThatResetMethodRecreateAllIndexes()
     {
-       $indexManager = new IndexManager(array(
-           'index_1' => new Index(),
-           'index_2' => new Index()
-       ), new Index());
-       
-       $reseter = new Reseter($indexManager);
-       $reseter->reset();
+        $indexManager = new IndexManager(array(
+            'index_1' => new Index(),
+            'index_2' => new Index()
+        ), new Index());
+        
+        $reseter = new Reseter($indexManager);
+        $reseter->reset();
 
-       $this->assertTrue($indexManager->getIndex('index_1')->created);
-       $this->assertTrue($indexManager->getIndex('index_2')->created);
+        $this->assertTrue($indexManager->getIndex('index_1')->created);
+        $this->assertTrue($indexManager->getIndex('index_2')->created);
     }
     
     public function testThatResetMethodCreateNewIndexes()
     {
-       $indexManager = new IndexManager(array(
-          'index_1' => new NewIndex(),
-          'index_2' => new NewIndex()
-       ), new NewIndex());
-       
-       $reseter = new Reseter($indexManager);
-       $reseter->reset();
+        $indexManager = new IndexManager(array(
+           'index_1' => new NewIndex(),
+           'index_2' => new NewIndex()
+        ), new NewIndex());
+        
+        $reseter = new Reseter($indexManager);
+        $reseter->reset();
 
-       $this->assertTrue($indexManager->getIndex('index_1')->created);
-       $this->assertTrue($indexManager->getIndex('index_2')->created);
+        $this->assertTrue($indexManager->getIndex('index_1')->created);
+        $this->assertTrue($indexManager->getIndex('index_2')->created);
     }
 }
