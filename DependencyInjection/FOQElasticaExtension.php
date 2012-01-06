ce82e8f591774ac857dc7809c310af3d486e4ab5
Jeremy Mikola <jmikola@gmail.com>
2012-01-06 15:23:54 -0800

    Merge pull request #62 from merk/patch-1
    Fix index wide finder config check

--- DependencyInjection/FOQElasticaExtension.php
+++ DependencyInjection/FOQElasticaExtension.php
@@ -111,7 +111,7 @@ class FOQElasticaExtension extends Extension
                     'mappings' => array()
                 )
             );
-            if (isset($index['finder'])) {
+            if ($index['finder']) {
                 $this->loadIndexFinder($container, $name, $indexId);
             }
             if (!empty($index['settings'])) {
