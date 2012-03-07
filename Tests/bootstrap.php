0391dff44eb73180af8c4279c8c8975bc90206ae
Jeremy Mikola <jmikola@gmail.com>
2012-03-06 20:30:37 -0500

    Configure Travis CI
    This also changes the test suite and bootstrap to depend on packages installed by composer.

---
+++ Tests/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+if (file_exists($file = __DIR__.'/../vendor/.composer/autoload.php')) {
+    $loader = require_once $file;
+} else {
+    throw new RuntimeException('Install dependencies to run test suite.');
+}
+
+spl_autoload_register(function($class) {
+    if (0 === strpos($class, 'FOQ\\ElasticaBundle\\')) {
+        $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 2)).'.php';
+        if (!stream_resolve_include_path($path)) {
+            return false;
+        }
+        require_once $path;
+        return true;
+    }
+});
