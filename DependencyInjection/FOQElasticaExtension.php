4593d9059801d34b50ec6429f87302637c9c7b1e
micjohnson <micjohnson@gmail.com>
2012-12-12 06:19:36 -0800

    Update DependencyInjection/FOQElasticaExtension.php

--- DependencyInjection/FOQElasticaExtension.php
+++ DependencyInjection/FOQElasticaExtension.php
@@ -167,14 +167,14 @@ class FOQElasticaExtension extends Extension
             $typeDef->setFactoryService($indexId);
             $typeDef->setFactoryMethod('getType');
             $container->setDefinition($typeId, $typeDef);
-            if (isset($type['_routing'])) {
-                $this->indexConfigs[$indexName]['config']['mappings'][$name]['_routing'] = $type['_routing'];
-            }
             if (isset($type['_source'])) {
                 $this->indexConfigs[$indexName]['config']['mappings'][$name]['_source'] = $type['_source'];
             }
             if (isset($type['_boost'])) {
                 $this->indexConfigs[$indexName]['config']['mappings'][$name]['_boost'] = $type['_boost'];
+            }
+            if (isset($type['_routing'])) {
+                $this->indexConfigs[$indexName]['config']['mappings'][$name]['_routing'] = $type['_routing'];
             }
             if (isset($type['mappings'])) {
                 $this->indexConfigs[$indexName]['config']['mappings'][$name]['properties'] = $type['mappings'];
