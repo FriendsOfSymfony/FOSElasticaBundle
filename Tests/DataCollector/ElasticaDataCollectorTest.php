f66858ff3ed2d94ec317725bafa94abc0e83312a
tyx <tim@amicalement-web.net>
2013-02-21 10:52:49 +0100

    Add test for the query time in datacollector

--- Tests/DataCollector/ElasticaDataCollectorTest.php
+++ Tests/DataCollector/ElasticaDataCollectorTest.php
@@ -60,4 +60,29 @@ class ElasticaDataCollectorTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($queries, $elasticaDataCollector->getQueries());
     }
 
+    public function testCorrectQueriesTime()
+    {
+        $requestMock = $this->getMockBuilder('Symfony\Component\HttpFoundation\Request')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $responseMock = $this->getMockBuilder('Symfony\Component\HttpFoundation\Response')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $loggerMock = $this->getMockBuilder('FOQ\ElasticaBundle\Logger\ElasticaLogger')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $queries = array(array('executionMS' => 10), array('executionMS' => 20));
+
+        $loggerMock->expects($this->once())
+            ->method('getQueries')
+            ->will($this->returnValue($queries));
+
+        $elasticaDataCollector = new ElasticaDataCollector($loggerMock);
+        $elasticaDataCollector->collect($requestMock, $responseMock);
+        $this->assertEquals(30, $elasticaDataCollector->getTime());
+    }
+
 }
