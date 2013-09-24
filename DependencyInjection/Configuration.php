16b48a4a5bceec10637f9406124acee7d22da1aa
Oleg Zinchenko <1cdecoder@gmail.com>
2013-09-24 17:05:43 +0300

    added "service_suffix" congig parameter

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -128,6 +128,7 @@ class Configuration implements ConfigurationInterface
                         ->performNoDeepMerging()
                         ->children()
                             ->scalarNode('index_name')->end()
+                            ->scalarNode('service_suffix')->end()
                             ->scalarNode('client')->end()
                             ->scalarNode('finder')
                                 ->treatNullLike(true)
