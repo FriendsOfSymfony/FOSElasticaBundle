753e9eeca49dca2848d0ac9b00ea2d33ec2cd1b9
caponica <git@caponica.com>
2013-11-05 17:51:36 +0000

    Enable pagination of hybrid results

---
+++ Paginator/HybridPartialResults.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace FOS\ElasticaBundle\Paginator;
+
+use FOS\ElasticaBundle\Transformer\ElasticaToModelTransformerInterface;
+use Elastica\ResultSet;
+
+/**
+ * Partial transformed result set
+ */
+class HybridPartialResults extends RawPartialResults
+{
+    protected $transformer;
+
+    /**
+     * @param ResultSet $resultSet
+     * @param \FOS\ElasticaBundle\Transformer\ElasticaToModelTransformerInterface $transformer
+     */
+    public function __construct(ResultSet $resultSet, ElasticaToModelTransformerInterface $transformer)
+    {
+        parent::__construct($resultSet);
+
+        $this->transformer = $transformer;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function toArray()
+    {
+        return $this->transformer->hybridTransform($this->resultSet->getResults());
+    }
+}
