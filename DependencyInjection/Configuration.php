39f1033a34e28ab6013e63654b9ec72b9865f3ff
Cassiano <cassianotartari@gmail.com>
2014-04-24 09:39:22 -0300

    Update Configuration.php
    Adding the option to set index_analyzer and search_analyzer to _all field.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -667,6 +667,8 @@ class Configuration implements ConfigurationInterface
         $node
             ->children()
             ->scalarNode('enabled')->defaultValue(true)->end()
+            ->scalarNode('index_analyzer')->end()
+            ->scalarNode('search_analyzer')->end()
             ->end()
         ;
 
