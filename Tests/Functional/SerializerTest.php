d57d430ab3e29a7502ac31500ed65f6c962a4674
tamirvs <tamirvs@gmail.com>
2014-06-18 17:02:50 +0000

    Ignore iterator keys when converting to array

---
+++ Tests/Functional/SerializerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of the FOSElasticaBundle project.
+ *
+ * (c) Tim Nagel <tim@nagel.com.au>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace FOS\ElasticaBundle\Tests\Functional;
+
+/**
+ * @group functional
+ */
+class SerializerTest extends WebTestCase
+{
+    public function testMappingIteratorToArrayField()
+    {
+        $client = $this->createClient(array('test_case' => 'Serializer'));
+        $persister = $client->getContainer()->get('fos_elastica.object_persister.index.type');
+
+        $object = new TypeObj();
+        $object->id = 1;
+        $object->coll = new \ArrayIterator(array('foo', 'bar'));
+        $persister->insertOne($object);
+
+        $object->coll = new \ArrayIterator(array('foo', 'bar', 'bazz'));
+        $object->coll->offsetUnset(1);
+
+        $persister->replaceOne($object);
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->deleteTmpDir('Serializer');
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        $this->deleteTmpDir('Serializer');
+    }
+}
