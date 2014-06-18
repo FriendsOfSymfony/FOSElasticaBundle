3ae382c9331462ca5543b3686444877e4a177fe5
Tim Nagel <tim@nagel.com.au>
2014-06-18 20:02:05 +1000

    Add tests to make sure KnpPaginator plays nice

--- Tests/Functional/app/Basic/bundles.php
+++ Tests/Functional/app/Basic/bundles.php
@@ -1,10 +1,13 @@
 <?php
 
 use FOS\ElasticaBundle\FOSElasticaBundle;
+use Knp\Bundle\PaginatorBundle\KnpPaginatorBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\TwigBundle\TwigBundle;
 
 return array(
     new FrameworkBundle(),
     new FOSElasticaBundle(),
-
+    new KnpPaginatorBundle(),
+    new TwigBundle(),
 );
