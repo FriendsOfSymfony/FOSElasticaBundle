27ceb6904d51eb8750d9f201214a0e47718501c0
Moritz Schwoerer <mr.mosch@gmail.com>
2014-11-14 23:11:28 +0100

    adding phpcr support
    prototype of phpcr support implemented

--- DependencyInjection/FOSElasticaExtension.php
+++ DependencyInjection/FOSElasticaExtension.php
@@ -520,6 +520,7 @@ class FOSElasticaExtension extends Extension
     {
         switch ($typeConfig['driver']) {
             case 'orm':
+            case 'phpcr':
                 $eventsClass = '\Doctrine\ORM\Events';
                 break;
             case 'mongodb':
