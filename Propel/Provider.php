e13cec447f9cd35cbd63880f564d6f23d7d22d90
Pablo Martelletti <pmartelletti@gmail.com>
2015-01-07 14:42:15 -0300

    Use ProgressBar in populate commande when available.

--- Propel/Provider.php
+++ Propel/Provider.php
@@ -20,6 +20,7 @@ class Provider extends AbstractProvider
         $nbObjects = $queryClass::create()->count();
         $offset = isset($options['offset']) ? intval($options['offset']) : 0;
         $sleep = isset($options['sleep']) ? intval($options['sleep']) : 0;
+        $progressBar = isset($options['progress-bar']) ? boolval($options['progress-bar']) : false;
         $batchSize = isset($options['batch-size']) ? intval($options['batch-size']) : $this->options['batch_size'];
 
         for (; $offset < $nbObjects; $offset += $batchSize) {
@@ -46,11 +47,13 @@ class Provider extends AbstractProvider
 
             usleep($sleep);
 
-            if ($loggerClosure) {
+            if ($loggerClosure && !$progressBar) {
                 $stepCount = $stepNbObjects + $offset;
                 $percentComplete = 100 * $stepCount / $nbObjects;
                 $objectsPerSecond = $stepNbObjects / (microtime(true) - $stepStartTime);
                 $loggerClosure(sprintf('%0.1f%% (%d/%d), %d objects/s %s', $percentComplete, $stepCount, $nbObjects, $objectsPerSecond, $this->getMemoryUsage()));
+            } else if ($loggerClosure && $progressBar) {
+                $loggerClosure($stepNbObjects);
             }
         }
     }
