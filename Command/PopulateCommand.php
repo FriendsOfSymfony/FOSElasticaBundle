14af7488408420618164b2c31f252e89259b2f70
Tim Nagel <tim@nagel.com.au>
2015-03-12 17:41:26 +1100

    Fix bad merge

--- Command/PopulateCommand.php
+++ Command/PopulateCommand.php
@@ -169,7 +169,7 @@ class PopulateCommand extends ContainerAwareCommand
         }
 
         $provider = $this->providerRegistry->getProvider($index, $type);
-        $loggerClosure = $this->getLoggerClosure($output, $index, $type);
+        $loggerClosure = $this->progressClosureBuilder->build($output, 'Populating', $index, $type);
         $provider->populate($loggerClosure, $event->getOptions());
 
         $this->dispatcher->dispatch(TypePopulateEvent::POST_TYPE_POPULATE, $event);
