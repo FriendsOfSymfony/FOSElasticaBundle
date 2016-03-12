d3b04f1b6dedba73e1d1a96a32ba97606cd7a0c6
Pierre Bonneau <pmithrandir@hotmail.com>
2016-03-12 15:34:43 +0200

    add support for result directly from elastic search without associating them with doctrine. Pagination available, 2nd commit

--- Finder/PaginatedFinderInterface.php
+++ Finder/PaginatedFinderInterface.php
@@ -27,4 +27,35 @@ interface PaginatedFinderInterface extends FinderInterface
      * @return PaginatorAdapterInterface
      */
     public function createPaginatorAdapter($query, $options = array());
+    
+    /**
+     * Searches for query results and returns them wrapped in a paginator.
+     *
+     * @param mixed $query   Can be a string, an array or an \Elastica\Query object
+     * @param array $options
+     *
+     * @return Pagerfanta paginated results
+     */
+    public function findRawPaginated($query, $options = array());
+    
+    /**
+     * Creates a paginator adapter for this query.
+     *
+     * @param mixed $query
+     * @param array $options
+     *
+     * @return PaginatorAdapterInterface
+     */
+    public function createRawPaginatorAdapter($query, $options = array());
+    
+    /**
+     * Searches for query results within a given limit.
+     *
+     * @param mixed $query   Can be a string, an array or an \Elastica\Query object
+     * @param int   $limit   How many results to get
+     * @param array $options
+     *
+     * @return array results
+     */
+    public function findRawResult($query, $limit = null, $options = array());
 }
