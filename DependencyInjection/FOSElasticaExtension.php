8568b9a45b879b1fadfacbd867fa264b3b6132c0
Christian Riesen <chris.riesen@gmail.com>
2016-03-17 16:36:13 +0100

    Add connectTimeout to options

--- DependencyInjection/FOSElasticaExtension.php
+++ DependencyInjection/FOSElasticaExtension.php
@@ -110,6 +110,11 @@ class FOSElasticaExtension extends Extension
             if (false !== $logger) {
                 $clientDef->addMethodCall('setLogger', array(new Reference($logger)));
             }
+
+            if (isset($clientConfig['connections'][0]['connectTimeout'])) {
+                $clientDef->addMethodCall('setConnectTimeout', [$clientConfig['connections'][0]['connectTimeout']]);
+            }
+
             $clientDef->addTag('fos_elastica.client');
 
             $container->setDefinition($clientId, $clientDef);
