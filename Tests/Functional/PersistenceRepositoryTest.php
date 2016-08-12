fazland/FazlandElasticaBundle

added test for persistence/repository configuration

Tests/Functional/PersistenceRepositoryTest.php
@@ -1,0 +1,31 @@
+<?php
+
+namespace Fazland\ElasticaBundle\Tests\Functional;
+
+class PersistenceRepositoryTest extends WebTestCase
+{
+    public function testRepositoryShouldBeSetCorrectly()
+    {
+        $client = $this->createClient(array('test_case' => 'ORM'));
+
+        $repository = $client->getContainer()->get('fazland_elastica.manager.orm')
+            ->getRepository('Fazland\ElasticaBundle\Tests\Functional\TypeObject');
+
+        $this->assertNotNull($repository);
+        $this->assertEquals('Fazland\ElasticaBundle\Tests\Functional\TypeObjectRepository', get_class($repository));
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->deleteTmpDir('Basic');
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        $this->deleteTmpDir('Basic');
+    }
+}

Tests/Functional/TypeObject.php
@@ -1,0 +1,36 @@
+<?php
+
+/**
+ * This file is part of the FazlandElasticaBundle project.
+ *
+ * (c) Infinite Networks Pty Ltd <http://www.infinite.net.au>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fazland\ElasticaBundle\Tests\Functional;
+
+class TypeObject
+{
+    public $id = 5;
+    public $coll;
+    public $field1;
+    public $field2;
+    public $field3;
+
+    public function isIndexable()
+    {
+        return true;
+    }
+
+    public function isntIndexable()
+    {
+        return false;
+    }
+
+    public function getSerializableColl()
+    {
+        return iterator_to_array($this->coll, false);
+    }
+}

Tests/Functional/TypeObjectRepository.php
@@ -1,0 +1,9 @@
+<?php
+
+namespace Fazland\ElasticaBundle\Tests\Functional;
+
+use Fazland\ElasticaBundle\Repository;
+
+class TypeObjectRepository extends Repository
+{
+}

FriendsOfSymfony/FOSElasticaBundle

Tests/Functional/PersistenceRepositoryTest.php
@@ -1,0 +1,31 @@
+<?php
+
+namespace FOS\ElasticaBundle\Tests\Functional;
+
+class PersistenceRepositoryTest extends WebTestCase
+{
+    public function testRepositoryShouldBeSetCorrectly()
+    {
+        $client = $this->createClient(array('test_case' => 'ORM'));
+
+        $repository = $client->getContainer()->get('fos_elastica.manager.orm')
+            ->getRepository('FOS\ElasticaBundle\Tests\Functional\TypeObject');
+
+        $this->assertNotNull($repository);
+        $this->assertEquals('FOS\ElasticaBundle\Tests\Functional\TypeObjectRepository', get_class($repository));
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->deleteTmpDir('Basic');
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        $this->deleteTmpDir('Basic');
+    }
+}

Tests/Functional/TypeObject.php
@@ -1,0 +1,36 @@
+<?php
+
+/**
+ * This file is part of the FOSElasticaBundle project.
+ *
+ * (c) Infinite Networks Pty Ltd <http://www.infinite.net.au>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FOS\ElasticaBundle\Tests\Functional;
+
+class TypeObject
+{
+    public $id = 5;
+    public $coll;
+    public $field1;
+    public $field2;
+    public $field3;
+
+    public function isIndexable()
+    {
+        return true;
+    }
+
+    public function isntIndexable()
+    {
+        return false;
+    }
+
+    public function getSerializableColl()
+    {
+        return iterator_to_array($this->coll, false);
+    }
+}

Tests/Functional/TypeObjectRepository.php
@@ -1,0 +1,9 @@
+<?php
+
+namespace FOS\ElasticaBundle\Tests\Functional;
+
+use FOS\ElasticaBundle\Repository;
+
+class TypeObjectRepository extends Repository
+{
+}