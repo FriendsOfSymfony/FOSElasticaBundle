fazland/FazlandElasticaBundle

refactoring repositoryManager: retrieve repository from "index/type" string

Doctrine/RepositoryManager.php
@@ -4,15 +4,19 @@ namespace Fazland\ElasticaBundle\Doctrine;
 
 use Doctrine\Common\Annotations\Reader;
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Fazland\ElasticaBundle\Finder\FinderInterface;
 use Fazland\ElasticaBundle\Manager\RepositoryManager as BaseManager;
+use Fazland\ElasticaBundle\Manager\RepositoryManagerInterface;
 
 /**
  * @author Richard Miller <info@limethinking.co.uk>
  *
  * Allows retrieval of basic or custom repository for mapped Doctrine
  * entities/documents.
+ *
+ * @deprecated
  */
-class RepositoryManager extends BaseManager
+class RepositoryManager implements RepositoryManagerInterface
 {
     /** @var array */
     protected $entities = array();
@@ -22,15 +26,34 @@ class RepositoryManager extends BaseManager
     
     /** @var ManagerRegistry */
     protected $managerRegistry;
+    /**
+     * @var RepositoryManagerInterface
+     */
+    private $repositoryManager;
 
     /**
      * @param ManagerRegistry $managerRegistry
-     * @param Reader          $reader
+     * @param RepositoryManagerInterface $repositoryManager
+     *
+     * @internal param Reader $reader
      */
-    public function __construct(ManagerRegistry $managerRegistry, Reader $reader)
+    public function __construct(ManagerRegistry $managerRegistry, RepositoryManagerInterface $repositoryManager)
     {
         $this->managerRegistry = $managerRegistry;
-        parent::__construct($reader);
+        $this->repositoryManager = $repositoryManager;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function addType($indexTypeName, FinderInterface $finder, $repositoryName = null)
+    {
+        throw new \LogicException(__METHOD__.' should not be called. Call addType on the main repository manager');
+    }
+
+    public function addEntity($entityName, $indexTypeName)
+    {
+        $this->entities[$entityName] = $indexTypeName;
     }
 
     /**
@@ -46,6 +69,10 @@ class RepositoryManager extends BaseManager
             $realEntityName = $this->managerRegistry->getAliasNamespace($namespaceAlias).'\\'.$simpleClassName;
         }
 
-        return parent::getRepository($realEntityName);
+        if (isset($this->entities[$realEntityName])) {
+            $realEntityName = $this->entities[$realEntityName];
+        }
+
+        return $this->repositoryManager->getRepository($realEntityName);
     }
 }

Manager/RepositoryManager.php
@@ -2,32 +2,38 @@
 
 namespace Fazland\ElasticaBundle\Manager;
 
-use Doctrine\Common\Annotations\Reader;
 use Fazland\ElasticaBundle\Finder\FinderInterface;
+use Fazland\ElasticaBundle\Repository;
 use RuntimeException;
 
 /**
- * @author Richard Miller <info@limethinking.co.uk>
- *
  * Allows retrieval of basic or custom repository for mapped Doctrine
  * entities/documents.
  */
 class RepositoryManager implements RepositoryManagerInterface
 {
-    protected $entities = array();
-    protected $repositories = array();
-    protected $reader;
+    /**
+     * @var array
+     */
+    private $types;
 
-    public function __construct(Reader $reader)
+    /**
+     * @var Repository[]
+     */
+    private $repositories;
+
+    public function __construct()
     {
-        $this->reader = $reader;
+        $this->types = [];
+        $this->repositories = [];
     }
 
-    public function addEntity($entityName, FinderInterface $finder, $repositoryName = null)
+    public function addType($indexTypeName, FinderInterface $finder, $repositoryName = null)
     {
-        $this->entities[$entityName] = array();
-        $this->entities[$entityName]['finder'] = $finder;
-        $this->entities[$entityName]['repositoryName'] = $repositoryName;
+        $this->types[$indexTypeName] = [
+            'finder' => $finder,
+            'repositoryName' => $repositoryName
+        ];
     }
 
     /**
@@ -35,57 +41,56 @@ class RepositoryManager implements RepositoryManagerInterface
      *
      * Returns custom repository if one specified otherwise
      * returns a basic repository.
+     *
+     * @param string $typeName
+     *
+     * @return Repository
      */
-    public function getRepository($entityName)
+    public function getRepository($typeName)
     {
-        if (isset($this->repositories[$entityName])) {
-            return $this->repositories[$entityName];
+        if (isset($this->repositories[$typeName])) {
+            return $this->repositories[$typeName];
         }
 
-        if (!isset($this->entities[$entityName])) {
-            throw new RuntimeException(sprintf('No search finder configured for %s', $entityName));
+        if (!isset($this->types[$typeName])) {
+            throw new RuntimeException(sprintf('No search finder configured for %s', $typeName));
         }
 
-        $repository = $this->createRepository($entityName);
-        $this->repositories[$entityName] = $repository;
+        $repository = $this->createRepository($typeName);
+        $this->repositories[$typeName] = $repository;
 
         return $repository;
     }
 
     /**
-     * @param string $entityName
+     * @param $typeName
      *
      * @return string
+     * @internal param string $entityName
+     *
      */
-    protected function getRepositoryName($entityName)
+    protected function getRepositoryName($typeName)
     {
-        if (isset($this->entities[$entityName]['repositoryName'])) {
-            return $this->entities[$entityName]['repositoryName'];
-        }
-
-        $refClass   = new \ReflectionClass($entityName);
-        $annotation = $this->reader->getClassAnnotation($refClass, 'Fazland\\ElasticaBundle\\Annotation\\Search');
-        if ($annotation) {
-            $this->entities[$entityName]['repositoryName']
-                = $annotation->repositoryClass;
-
-            return $annotation->repositoryClass;
+        if (isset($this->types[$typeName]['repositoryName'])) {
+            return $this->types[$typeName]['repositoryName'];
         }
 
         return 'Fazland\ElasticaBundle\Repository';
     }
 
     /**
-     * @param string $entityName
+     * @param $typeName
      *
      * @return mixed
+     * @internal param string $entityName
+     *
      */
-    private function createRepository($entityName)
+    private function createRepository($typeName)
     {
-        if (!class_exists($repositoryName = $this->getRepositoryName($entityName))) {
-            throw new RuntimeException(sprintf('%s repository for %s does not exist', $repositoryName, $entityName));
+        if (!class_exists($repositoryName = $this->getRepositoryName($typeName))) {
+            throw new RuntimeException(sprintf('%s repository for %s does not exist', $repositoryName, $typeName));
         }
 
-        return new $repositoryName($this->entities[$entityName]['finder']);
+        return new $repositoryName($this->types[$typeName]['finder']);
     }
 }

Manager/RepositoryManagerInterface.php
@@ -3,6 +3,7 @@
 namespace Fazland\ElasticaBundle\Manager;
 
 use Fazland\ElasticaBundle\Finder\FinderInterface;
+use Fazland\ElasticaBundle\Repository;
 
 /**
  * @author Richard Miller <info@limethinking.co.uk>
@@ -13,14 +14,14 @@ use Fazland\ElasticaBundle\Finder\FinderInterface;
 interface RepositoryManagerInterface
 {
     /**
-     * Adds entity name and its finder.
+     * Adds type name and its finder.
      * Custom repository class name can also be added.
      *
-     * @param string $entityName
+     * @param string $indexTypeName The type name in "index/type" format
      * @param        $finder
      * @param string $repositoryName
      */
-    public function addEntity($entityName, FinderInterface $finder, $repositoryName = null);
+    public function addType($indexTypeName, FinderInterface $finder, $repositoryName = null);
 
     /**
      * Return repository for entity.
@@ -28,7 +29,9 @@ interface RepositoryManagerInterface
      * Returns custom repository if one specified otherwise
      * returns a basic repository.
      *
-     * @param string $entityName
+     * @param $typeName
+     *
+     * @return Repository
      */
-    public function getRepository($entityName);
+    public function getRepository($typeName);
 }

Tests/Manager/RepositoryManagerTest.php
@@ -29,11 +29,11 @@ class RepositoryManagerTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMock();
 
-        $entityName = 'Fazland\ElasticaBundle\Tests\Manager\Entity';
+        $typeName = 'index/type';
 
         $manager = new RepositoryManager($readerMock);
-        $manager->addEntity($entityName, $finderMock);
-        $repository = $manager->getRepository($entityName);
+        $manager->addType($typeName, $finderMock);
+        $repository = $manager->getRepository($typeName);
         $this->assertInstanceOf('Fazland\ElasticaBundle\Repository', $repository);
     }
 
@@ -49,11 +49,11 @@ class RepositoryManagerTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMock();
 
-        $entityName = 'Fazland\ElasticaBundle\Tests\Manager\Entity';
+        $typeName = 'index/type';
 
         $manager = new RepositoryManager($readerMock);
-        $manager->addEntity($entityName, $finderMock, 'Fazland\ElasticaBundle\Tests\Manager\CustomRepository');
-        $repository = $manager->getRepository($entityName);
+        $manager->addType($typeName, $finderMock, 'Fazland\ElasticaBundle\Tests\Manager\CustomRepository');
+        $repository = $manager->getRepository($typeName);
         $this->assertInstanceOf('Fazland\ElasticaBundle\Tests\Manager\CustomRepository', $repository);
     }
 
@@ -72,11 +72,11 @@ class RepositoryManagerTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMock();
 
-        $entityName = 'Fazland\ElasticaBundle\Tests\Manager\Entity';
+        $typeName = 'index/type';
 
         $manager = new RepositoryManager($readerMock);
-        $manager->addEntity($entityName, $finderMock);
-        $manager->getRepository('Missing Entity');
+        $manager->addType($typeName, $finderMock);
+        $manager->getRepository('Missing type');
     }
 
     /**
@@ -94,10 +94,10 @@ class RepositoryManagerTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMock();
 
-        $entityName = 'Fazland\ElasticaBundle\Tests\Manager\Entity';
+        $typeName = 'index/type';
 
         $manager = new RepositoryManager($readerMock);
-        $manager->addEntity($entityName, $finderMock, 'Fazland\ElasticaBundle\Tests\MissingRepository');
-        $manager->getRepository('Missing Entity');
+        $manager->addType($typeName, $finderMock, 'Fazland\ElasticaBundle\Tests\MissingRepository');
+        $manager->getRepository($typeName);
     }
 }

FriendsOfSymfony/FOSElasticaBundle

Doctrine/RepositoryManager.php
@@ -4,15 +4,19 @@ namespace FOS\ElasticaBundle\Doctrine;
 
 use Doctrine\Common\Annotations\Reader;
 use Doctrine\Common\Persistence\ManagerRegistry;
+use FOS\ElasticaBundle\Finder\FinderInterface;
 use FOS\ElasticaBundle\Manager\RepositoryManager as BaseManager;
+use FOS\ElasticaBundle\Manager\RepositoryManagerInterface;
 
 /**
  * @author Richard Miller <info@limethinking.co.uk>
  *
  * Allows retrieval of basic or custom repository for mapped Doctrine
  * entities/documents.
+ *
+ * @deprecated
  */
-class RepositoryManager extends BaseManager
+class RepositoryManager implements RepositoryManagerInterface
 {
     /** @var array */
     protected $entities = array();
@@ -24,13 +28,31 @@ class RepositoryManager extends BaseManager
     protected $managerRegistry;
 
     /**
-     * @param ManagerRegistry $managerRegistry
-     * @param Reader          $reader
+     * @var RepositoryManagerInterface
      */
-    public function __construct(ManagerRegistry $managerRegistry, Reader $reader)
+    private $repositoryManager;
+
+    /**
+     * @param ManagerRegistry $managerRegistry
+     * @param RepositoryManagerInterface $repositoryManager
+     */
+    public function __construct(ManagerRegistry $managerRegistry, RepositoryManagerInterface $repositoryManager)
     {
         $this->managerRegistry = $managerRegistry;
-        parent::__construct($reader);
+        $this->repositoryManager = $repositoryManager;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function addType($indexTypeName, FinderInterface $finder, $repositoryName = null)
+    {
+        throw new \LogicException(__METHOD__.' should not be called. Call addType on the main repository manager');
+    }
+
+    public function addEntity($entityName, $indexTypeName)
+    {
+        $this->entities[$entityName] = $indexTypeName;
     }
 
     /**
@@ -46,6 +68,10 @@ class RepositoryManager extends BaseManager
             $realEntityName = $this->managerRegistry->getAliasNamespace($namespaceAlias).'\\'.$simpleClassName;
         }
 
-        return parent::getRepository($realEntityName);
+        if (isset($this->entities[$realEntityName])) {
+            $realEntityName = $this->entities[$realEntityName];
+        }
+
+        return $this->repositoryManager->getRepository($realEntityName);
     }
 }

Manager/RepositoryManager.php
@@ -4,6 +4,7 @@ namespace FOS\ElasticaBundle\Manager;
 
 use Doctrine\Common\Annotations\Reader;
 use FOS\ElasticaBundle\Finder\FinderInterface;
+use FOS\ElasticaBundle\Repository;
 use RuntimeException;
 
 /**
@@ -14,20 +15,28 @@ use RuntimeException;
  */
 class RepositoryManager implements RepositoryManagerInterface
 {
-    protected $entities = array();
-    protected $repositories = array();
-    protected $reader;
+    /**
+     * @var array
+     */
+    private $types;
 
-    public function __construct(Reader $reader)
+    /**
+     * @var Repository[]
+     */
+    private $repositories;
+
+    public function __construct()
     {
-        $this->reader = $reader;
+        $this->types = [];
+        $this->repositories = [];
     }
 
-    public function addEntity($entityName, FinderInterface $finder, $repositoryName = null)
+    public function addType($indexTypeName, FinderInterface $finder, $repositoryName = null)
     {
-        $this->entities[$entityName] = array();
-        $this->entities[$entityName]['finder'] = $finder;
-        $this->entities[$entityName]['repositoryName'] = $repositoryName;
+        $this->types[$indexTypeName] = [
+            'finder' => $finder,
+            'repositoryName' => $repositoryName
+        ];
     }
 
     /**
@@ -35,57 +44,52 @@ class RepositoryManager implements RepositoryManagerInterface
      *
      * Returns custom repository if one specified otherwise
      * returns a basic repository.
+     *
+     * @param string $typeName
+     *
+     * @return Repository
      */
-    public function getRepository($entityName)
+    public function getRepository($typeName)
     {
-        if (isset($this->repositories[$entityName])) {
-            return $this->repositories[$entityName];
+        if (isset($this->repositories[$typeName])) {
+            return $this->repositories[$typeName];
         }
 
-        if (!isset($this->entities[$entityName])) {
-            throw new RuntimeException(sprintf('No search finder configured for %s', $entityName));
+        if (!isset($this->types[$typeName])) {
+            throw new RuntimeException(sprintf('No search finder configured for %s', $typeName));
         }
 
-        $repository = $this->createRepository($entityName);
-        $this->repositories[$entityName] = $repository;
+        $repository = $this->createRepository($typeName);
+        $this->repositories[$typeName] = $repository;
 
         return $repository;
     }
 
     /**
-     * @param string $entityName
+     * @param $typeName
      *
      * @return string
      */
-    protected function getRepositoryName($entityName)
+    protected function getRepositoryName($typeName)
     {
-        if (isset($this->entities[$entityName]['repositoryName'])) {
-            return $this->entities[$entityName]['repositoryName'];
-        }
-
-        $refClass   = new \ReflectionClass($entityName);
-        $annotation = $this->reader->getClassAnnotation($refClass, 'FOS\\ElasticaBundle\\Annotation\\Search');
-        if ($annotation) {
-            $this->entities[$entityName]['repositoryName']
-                = $annotation->repositoryClass;
-
-            return $annotation->repositoryClass;
+        if (isset($this->types[$typeName]['repositoryName'])) {
+            return $this->types[$typeName]['repositoryName'];
         }
 
         return 'FOS\ElasticaBundle\Repository';
     }
 
     /**
-     * @param string $entityName
+     * @param $typeName
      *
      * @return mixed
      */
-    private function createRepository($entityName)
+    private function createRepository($typeName)
     {
-        if (!class_exists($repositoryName = $this->getRepositoryName($entityName))) {
-            throw new RuntimeException(sprintf('%s repository for %s does not exist', $repositoryName, $entityName));
+        if (!class_exists($repositoryName = $this->getRepositoryName($typeName))) {
+            throw new RuntimeException(sprintf('%s repository for %s does not exist', $repositoryName, $typeName));
         }
 
-        return new $repositoryName($this->entities[$entityName]['finder']);
+        return new $repositoryName($this->types[$typeName]['finder']);
     }
 }

Manager/RepositoryManagerInterface.php
@@ -3,6 +3,7 @@
 namespace FOS\ElasticaBundle\Manager;
 
 use FOS\ElasticaBundle\Finder\FinderInterface;
+use FOS\ElasticaBundle\Repository;
 
 /**
  * @author Richard Miller <info@limethinking.co.uk>
@@ -13,14 +14,14 @@ use FOS\ElasticaBundle\Finder\FinderInterface;
 interface RepositoryManagerInterface
 {
     /**
-     * Adds entity name and its finder.
+     * Adds type name and its finder.
      * Custom repository class name can also be added.
      *
-     * @param string $entityName
+     * @param string $indexTypeName The type name in "index/type" format
      * @param        $finder
      * @param string $repositoryName
      */
-    public function addEntity($entityName, FinderInterface $finder, $repositoryName = null);
+    public function addType($indexTypeName, FinderInterface $finder, $repositoryName = null);
 
     /**
      * Return repository for entity.
@@ -28,7 +29,9 @@ interface RepositoryManagerInterface
      * Returns custom repository if one specified otherwise
      * returns a basic repository.
      *
-     * @param string $entityName
+     * @param $typeName
+     *
+     * @return Repository
      */
-    public function getRepository($entityName);
+    public function getRepository($typeName);
 }

Tests/Manager/RepositoryManagerTest.php
@@ -2,7 +2,6 @@
 
 namespace FOS\ElasticaBundle\Tests\Manager;
 
-use FOS\ElasticaBundle\Configuration\Search;
 use FOS\ElasticaBundle\Manager\RepositoryManager;
 
 class CustomRepository
@@ -18,53 +17,43 @@ class Entity
  */
 class RepositoryManagerTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @return \PHPUnit_Framework_MockObject_MockObject|\FOS\ElasticaBundle\Finder\TransformedFinder
-     */
-    private function createFinderMock()
+    public function testThatGetRepositoryReturnsDefaultRepository()
     {
+        /** @var $finderMock \PHPUnit_Framework_MockObject_MockObject|\FOS\ElasticaBundle\Finder\TransformedFinder */
         $finderMock = $this->getMockBuilder('FOS\ElasticaBundle\Finder\TransformedFinder')
             ->disableOriginalConstructor()
             ->getMock();
 
-        return $finderMock;
-    }
-
-    /**
-     * @return \PHPUnit_Framework_MockObject_MockObject|\Doctrine\Common\Annotations\Reader
-     */
-    private function createReaderMock()
-    {
+        /** @var $readerMock \PHPUnit_Framework_MockObject_MockObject|\Doctrine\Common\Annotations\Reader */
         $readerMock = $this->getMockBuilder('Doctrine\Common\Annotations\Reader')
             ->disableOriginalConstructor()
             ->getMock();
 
-        return $readerMock;
-    }
-
-    public function testThatGetRepositoryReturnsDefaultRepository()
-    {
-        $finderMock = $this->createFinderMock();
-        $readerMock = $this->createReaderMock();
-
-        $entityName = 'FOS\ElasticaBundle\Tests\Manager\Entity';
+        $typeName = 'index/type';
 
         $manager = new RepositoryManager($readerMock);
-        $manager->addEntity($entityName, $finderMock);
-        $repository = $manager->getRepository($entityName);
+        $manager->addType($typeName, $finderMock);
+        $repository = $manager->getRepository($typeName);
         $this->assertInstanceOf('FOS\ElasticaBundle\Repository', $repository);
     }
 
     public function testThatGetRepositoryReturnsCustomRepository()
     {
-        $finderMock = $this->createFinderMock();
-        $readerMock = $this->createReaderMock();
+        /** @var $finderMock \PHPUnit_Framework_MockObject_MockObject|\FOS\ElasticaBundle\Finder\TransformedFinder */
+        $finderMock = $this->getMockBuilder('FOS\ElasticaBundle\Finder\TransformedFinder')
+            ->disableOriginalConstructor()
+            ->getMock();
 
-        $entityName = 'FOS\ElasticaBundle\Tests\Manager\Entity';
+        /** @var $readerMock \PHPUnit_Framework_MockObject_MockObject|\Doctrine\Common\Annotations\Reader */
+        $readerMock = $this->getMockBuilder('Doctrine\Common\Annotations\Reader')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $typeName = 'index/type';
 
         $manager = new RepositoryManager($readerMock);
-        $manager->addEntity($entityName, $finderMock, 'FOS\ElasticaBundle\Tests\Manager\CustomRepository');
-        $repository = $manager->getRepository($entityName);
+        $manager->addType($typeName, $finderMock, 'FOS\ElasticaBundle\Tests\Manager\CustomRepository');
+        $repository = $manager->getRepository($typeName);
         $this->assertInstanceOf('FOS\ElasticaBundle\Tests\Manager\CustomRepository', $repository);
     }
 
@@ -73,14 +62,21 @@ class RepositoryManagerTest extends \PHPUnit_Framework_TestCase
      */
     public function testThatGetRepositoryThrowsExceptionIfEntityNotConfigured()
     {
-        $finderMock = $this->createFinderMock();
-        $readerMock = $this->createReaderMock();
+        /** @var $finderMock \PHPUnit_Framework_MockObject_MockObject|\FOS\ElasticaBundle\Finder\TransformedFinder */
+        $finderMock = $this->getMockBuilder('FOS\ElasticaBundle\Finder\TransformedFinder')
+            ->disableOriginalConstructor()
+            ->getMock();
 
-        $entityName = 'FOS\ElasticaBundle\Tests\Manager\Entity';
+        /** @var $readerMock \PHPUnit_Framework_MockObject_MockObject|\Doctrine\Common\Annotations\Reader */
+        $readerMock = $this->getMockBuilder('Doctrine\Common\Annotations\Reader')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $typeName = 'index/type';
 
         $manager = new RepositoryManager($readerMock);
-        $manager->addEntity($entityName, $finderMock);
-        $manager->getRepository('Missing Entity');
+        $manager->addType($typeName, $finderMock);
+        $manager->getRepository('Missing type');
     }
 
     /**
@@ -88,70 +84,20 @@ class RepositoryManagerTest extends \PHPUnit_Framework_TestCase
      */
     public function testThatGetRepositoryThrowsExceptionIfCustomRepositoryNotFound()
     {
-        $finderMock = $this->createFinderMock();
-        $readerMock = $this->createReaderMock();
+        /** @var $finderMock \PHPUnit_Framework_MockObject_MockObject|\FOS\ElasticaBundle\Finder\TransformedFinder */
+        $finderMock = $this->getMockBuilder('FOS\ElasticaBundle\Finder\TransformedFinder')
+            ->disableOriginalConstructor()
+            ->getMock();
 
-        $entityName = 'FOS\ElasticaBundle\Tests\Manager\Entity';
+        /** @var $readerMock \PHPUnit_Framework_MockObject_MockObject|\Doctrine\Common\Annotations\Reader */
+        $readerMock = $this->getMockBuilder('Doctrine\Common\Annotations\Reader')
+            ->disableOriginalConstructor()
+            ->getMock();
 
-        $manager = new RepositoryManager($readerMock);
-        $manager->addEntity($entityName, $finderMock, 'FOS\ElasticaBundle\Tests\MissingRepository');
-        $manager->getRepository('FOS\ElasticaBundle\Tests\Manager\Entity');
-    }
-
-    /**
-     * @expectedException \RuntimeException
-     */
-    public function testThatGetRepositoryThrowsExceptionIfEntityDoesNotExist()
-    {
-        $finderMock = $this->createFinderMock();
-        $readerMock = $this->createReaderMock();
-
-        $entityName = 'FOS\ElasticaBundle\Tests\Manager\Entity';
+        $typeName = 'index/type';
 
         $manager = new RepositoryManager($readerMock);
-        $manager->addEntity($entityName, $finderMock, 'FOS\ElasticaBundle\Tests\MissingRepository');
-        $manager->getRepository('Missing Entity');
-    }
-
-    public function testThatGetRepositoryCachesRepositoryInstances()
-    {
-        $finderMock = $this->createFinderMock();
-        $readerMock = $this->createReaderMock();
-
-        $entityName = 'FOS\ElasticaBundle\Tests\Manager\Entity';
-
-        $manager = new RepositoryManager($readerMock);
-        $manager->addEntity($entityName, $finderMock, 'FOS\ElasticaBundle\Tests\Manager\CustomRepository');
-        $repository = $manager->getRepository($entityName);
-        $this->assertInstanceOf('FOS\ElasticaBundle\Tests\Manager\CustomRepository', $repository);
-
-        $repository2 = $manager->getRepository($entityName);
-        $this->assertInstanceOf('FOS\ElasticaBundle\Tests\Manager\CustomRepository', $repository2);
-        $this->assertSame($repository, $repository2);
-    }
-
-    public function testGetRepositoryNameCanReadFromClassAnnotation()
-    {
-        $repositoryClass = 'FOS\ElasticaBundle\Tests\Manager\CustomRepository';
-
-        $annotation = new Search;
-        $annotation->repositoryClass = $repositoryClass;
-
-        $finderMock = $this->createFinderMock();
-        $readerMock = $this->createReaderMock();
-
-        $readerMock
-            ->expects($this->once())
-            ->method('getClassAnnotation')
-            ->will($this->returnValue($annotation));
-
-        $entityName = 'FOS\ElasticaBundle\Tests\Manager\Entity';
-
-        $manager = new RepositoryManager($readerMock);
-        $manager->addEntity($entityName, $finderMock);
-
-        $repository = $manager->getRepository($entityName);
-
-        $this->assertInstanceOf($repositoryClass, $repository);
+        $manager->addType($typeName, $finderMock, 'FOS\ElasticaBundle\Tests\MissingRepository');
+        $manager->getRepository($typeName);
     }
 }