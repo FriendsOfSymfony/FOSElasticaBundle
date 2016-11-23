5a1d609c14ae97fc6b2adc6bdbfbfe14301f0d63
Alessandro Chitolina <alessandro.chitolina@fazland.com>
2016-11-23 13:30:45 +0100

    fixed tests with serializer

--- Tests/Functional/TypeObj.php
+++ Tests/Functional/TypeObj.php
@@ -30,6 +30,10 @@ class TypeObj
 
     public function getSerializableColl()
     {
+        if (null === $this->coll) {
+            return null;
+        }
+
         return iterator_to_array($this->coll, false);
     }
 }
