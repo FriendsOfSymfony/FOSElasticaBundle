df72793de7849d26340ba2a2de23c237e7e15b5f
Vic D'Elfant <vic@wijzijnmiddag.nl>
2017-12-09 19:09:23 +0100

    Cast identifier value to string
    Cast non-scalar identifiers to string
    
    Improved earlier changes by checking for non-scalar identifiers

--- Tests/Transformer/ModelToElasticaAutoTransformerTest.php
+++ Tests/Transformer/ModelToElasticaAutoTransformerTest.php
@@ -531,6 +531,20 @@ class ModelToElasticaAutoTransformerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('bar', $data['unmappedValue']);
     }
 
+    public function testIdentifierIsCastedToString()
+    {
+        $idObject = new CastableObject();;
+        $idObject->foo = '00000000-0000-0000-0000-000000000000';
+
+        $object = new \stdClass();
+        $object->id = $idObject;
+
+        $transformer = $this->getTransformer();
+        $document = $transformer->transform($object, []);
+
+        $this->assertSame('string', gettype($document->getId()));
+    }
+
     /**
      * @param null|\Symfony\Component\EventDispatcher\EventDispatcherInterface $dispatcher
      *
