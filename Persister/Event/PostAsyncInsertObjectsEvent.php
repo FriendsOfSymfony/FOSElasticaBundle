9dbdfce4fdc648a078eed74fb79757e0cfb2a5a0
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-12-11 17:47:19 +0200

    add post async insert objects event

---
+++ Persister/Event/PostAsyncInsertObjectsEvent.php
@@ -0,0 +1,83 @@
+<?php
+namespace FOS\ElasticaBundle\Persister\Event;
+
+use FOS\ElasticaBundle\Persister\ObjectPersisterInterface;
+use FOS\ElasticaBundle\Provider\PagerInterface;
+use Symfony\Component\EventDispatcher\Event;
+
+final class PostAsyncInsertObjectsEvent extends Event implements PersistEvent
+{
+    /**
+     * @var PagerInterface
+     */
+    private $pager;
+
+    /**
+     * @var ObjectPersisterInterface
+     */
+    private $objectPersister;
+
+    /**
+     * @var int
+     */
+    private $objectsCount;
+
+    /**
+     * @var string|null
+     */
+    private $errorMessage;
+
+    /**
+     * @var array
+     */
+    private $options;
+
+    public function __construct(PagerInterface $pager, ObjectPersisterInterface $objectPersister, $objectsCount, $errorMessage, array $options)
+    {
+        $this->pager = $pager;
+        $this->objectPersister = $objectPersister;
+        $this->objectsCount = $objectsCount;
+        $this->errorMessage = $errorMessage;
+        $this->options = $options;
+    }
+
+    /**
+     * @return PagerInterface
+     */
+    public function getPager()
+    {
+        return $this->pager;
+    }
+
+    /**
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * @return ObjectPersisterInterface
+     */
+    public function getObjectPersister()
+    {
+        return $this->objectPersister;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getErrorMessage()
+    {
+        return $this->errorMessage;
+    }
+
+    /**
+     * @return int
+     */
+    public function getObjectsCount()
+    {
+        return $this->objectsCount;
+    }
+}
