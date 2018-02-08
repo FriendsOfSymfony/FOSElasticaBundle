bfae328023fc000f4fe06556737b8d55cc2e53e1
Christophe Coevoet <stof@notk.org>
2018-02-08 14:44:51 +0100

    Remove bad duplicate license headers

--- src/Command/ProgressClosureBuilder.php
+++ src/Command/ProgressClosureBuilder.php
@@ -9,15 +9,6 @@
  * file that was distributed with this source code.
  */
 
-/**
- * This file is part of the FOSElasticaBundle project.
- *
- * (c) Tim Nagel <tim@nagel.com.au>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace FOS\ElasticaBundle\Command;
 
 use Symfony\Component\Console\Helper\ProgressBar;
--- src/Configuration/Source/ContainerSource.php
+++ src/Configuration/Source/ContainerSource.php
@@ -9,15 +9,6 @@
  * file that was distributed with this source code.
  */
 
-/**
- * This file is part of the FOSElasticaBundle project.
- *
- * (c) Tim Nagel <tim@nagel.com.au>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace FOS\ElasticaBundle\Configuration\Source;
 
 use FOS\ElasticaBundle\Configuration\IndexConfig;
