db424038878c6fab3483ce40e446dcd57cf93f3c
Rimas Kudelis <rimas@kudelis.lt>
2020-05-02 21:48:43 +0300

    Support for queued messages via Symfony Messenger
    Kinda fixes #1586, but probably still needs tests and whatnot.

---
+++ src/Persister/AsyncPagerPersister.php
@@ -0,0 +1,89 @@
+<?php
+declare(strict_types=1);
+
+namespace FOS\ElasticaBundle\Persister;
+
+use FOS\ElasticaBundle\Message\AsyncPersistPage;
+use FOS\ElasticaBundle\Provider\PagerInterface;
+use FOS\ElasticaBundle\Provider\PagerProviderRegistry;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+final class AsyncPagerPersister implements PagerPersisterInterface
+{
+    const NAME = 'async';
+    const DEFAULT_PAGE_SIZE = 100;
+
+    /**
+     * @var PagerPersisterRegistry
+     */
+    private $pagerPersisterRegistry;
+
+    /**
+     * @var PagerProviderRegistry
+     */
+    private $pagerProviderRegistry;
+
+    /**
+     * @var MessageBusInterface
+     */
+    private $messageBus;
+
+    public function __construct(
+        PagerPersisterRegistry $pagerPersisterRegistry,
+        PagerProviderRegistry $pagerProviderRegistry,
+        MessageBusInterface $messageBus
+    ) {
+        $this->pagerPersisterRegistry = $pagerPersisterRegistry;
+        $this->pagerProviderRegistry = $pagerProviderRegistry;
+        $this->messageBus = $messageBus;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function insert(PagerInterface $pager, array $options = array()): void
+    {
+        $pager->setMaxPerPage(empty($options['max_per_page']) ? self::DEFAULT_PAGE_SIZE : $options['max_per_page']);
+
+        $options = array_replace([
+            'max_per_page' => $pager->getMaxPerPage(),
+            'first_page' => $pager->getCurrentPage(),
+            'last_page' => $pager->getNbPages(),
+        ], $options);
+
+        $pager->setCurrentPage($options['first_page']);
+
+        $lastPage = min($options['last_page'], $pager->getNbPages());
+        $page = $pager->getCurrentPage();
+        do {
+            $this->messageBus->dispatch(new AsyncPersistPage($page, $options));
+
+            $page++;
+        } while ($page <= $lastPage);
+    }
+
+    public function insertPage(int $page, array $options = []): int
+    {
+        if (!isset($options['indexName'])) {
+            throw new \RuntimeException('Invalid call. $options is missing the indexName key.');
+        }
+        if (!isset($options['max_per_page'])) {
+            throw new \RuntimeException('Invalid call. $options is missing the max_per_page key.');
+        }
+
+        $options['first_page'] = $page;
+        $options['last_page'] = $page;
+
+        $provider = $this->pagerProviderRegistry->getProvider($options['indexName']);
+        $pager = $provider->provide($options);
+        $pager->setMaxPerPage($options['max_per_page']);
+        $pager->setCurrentPage($options['first_page']);
+
+        $objectCount = count($pager->getCurrentPageResults());
+
+        $pagerPersister = $this->pagerPersisterRegistry->getPagerPersister(InPlacePagerPersister::NAME);
+        $pagerPersister->insert($pager, $options);
+
+        return $objectCount;
+    }
+}
