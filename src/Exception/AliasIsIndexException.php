fece6e2ee88b03716b8d1948d4c522888b0fa764
François-Xavier de Guillebon <deguif@gmail.com>
2020-09-14 19:51:43 +0200

    Clean exceptions

--- src/Exception/AliasIsIndexException.php
+++ src/Exception/AliasIsIndexException.php
@@ -13,10 +13,7 @@ namespace FOS\ElasticaBundle\Exception;
 
 class AliasIsIndexException extends \Exception
 {
-    /**
-     * @param string $indexName
-     */
-    public function __construct($indexName)
+    public function __construct(string $indexName)
     {
         parent::__construct(sprintf('Expected %s to be an alias but it is an index.', $indexName));
     }
