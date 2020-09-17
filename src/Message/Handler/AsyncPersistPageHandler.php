26df7c536782d001832fceb5faced121620648b1
François-Xavier de Guillebon <deguif@gmail.com>
2020-09-17 02:27:32 +0200

    Fix CS

--- src/Message/Handler/AsyncPersistPageHandler.php
+++ src/Message/Handler/AsyncPersistPageHandler.php
@@ -1,5 +1,13 @@
 <?php
-declare(strict_types=1);
+
+/*
+ * This file is part of the FOSElasticaBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace FOS\ElasticaBundle\Message\Handler;
 
@@ -10,7 +18,7 @@ use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 class AsyncPersistPageHandler implements MessageHandlerInterface
 {
     /**
-     * var AsyncMessagePersister
+     * @var AsyncPagerPersister
      */
     private $persister;
 
