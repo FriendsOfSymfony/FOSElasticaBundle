97391b8c1fe0587d06bf5804dabab58391517255
François-Xavier de Guillebon <deguif@gmail.com>
2020-09-17 09:38:02 +0200

    Update headers

--- tests/Unit/Persister/AsyncPagerPersisterTest.php
+++ tests/Unit/Persister/AsyncPagerPersisterTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of the FOSElasticaBundle package.
  *
- * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ * (c) FriendsOfSymfony <https://friendsofsymfony.github.com/>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
