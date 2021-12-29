49ea5ac161da95d8d7d2cd0cebb8b207de9b7bc6
Karel <karelke@gmail.com>
2021-12-29 13:57:19 +0100

    Add return type to Command::execute()

--- src/Command/PopulateCommand.php
+++ src/Command/PopulateCommand.php
@@ -117,10 +117,7 @@ class PopulateCommand extends Command
         }
     }
 
-    /**
-     * @return int
-     */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $indexes = (null !== $index = $input->getOption('index')) ? [$index] : \array_keys($this->indexManager->getAllIndexes());
         $reset = !$input->getOption('no-reset');
