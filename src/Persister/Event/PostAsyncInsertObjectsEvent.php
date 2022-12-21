14d3210ed6807edfdfc228079db156545cbbc249
Bob van de Vijver <bobvandevijver@hotmail.com>
2022-12-21 16:10:07 +0100

    Deprecate PostAsyncInsertObjectsEvent as it is not emitted by the bundle

---
+++ src/Persister/Event/PostAsyncInsertObjectsEvent.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+ * This file is part of the FOSElasticaBundle package.
+ *
+ * (c) FriendsOfSymfony <https://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FOS\ElasticaBundle\Persister\Event;
+
+use FOS\ElasticaBundle\Persister\ObjectPersisterInterface;
+use FOS\ElasticaBundle\Provider\PagerInterface;
+use Symfony\Contracts\EventDispatcher\Event;
+
+@\trigger_error(\sprintf('The %s class is deprecated since version 6.3 and will be removed in 7.0.', PostAsyncInsertObjectsEvent::class), \E_USER_DEPRECATED);
+
+/**
+ * @deprecated since 6.3 will be removed in 7.0
+ */
+final class PostAsyncInsertObjectsEvent extends Event implements PersistEvent
+{
+    /**
+     * @var PagerInterface
+     */
+    private $pager;
+
+    /**
+     * @var ObjectPersisterInterface
+     */
+    private $objectPersister;
+
+    /**
+     * @var int
+     */
+    private $objectsCount;
+
+    /**
+     * @var string|null
+     */
+    private $errorMessage;
+
+    /**
+     * @var array<string, mixed>
+     */
+    private $options;
+
+    /**
+     * @param array<string, mixed> $options
+     */
+    public function __construct(PagerInterface $pager, ObjectPersisterInterface $objectPersister, int $objectsCount, ?string $errorMessage, array $options)
+    {
+        $this->pager = $pager;
+        $this->objectPersister = $objectPersister;
+        $this->objectsCount = $objectsCount;
+        $this->errorMessage = $errorMessage;
+        $this->options = $options;
+    }
+
+    public function getPager(): PagerInterface
+    {
+        return $this->pager;
+    }
+
+    public function getOptions(): array
+    {
+        return $this->options;
+    }
+
+    public function getObjectPersister(): ObjectPersisterInterface
+    {
+        return $this->objectPersister;
+    }
+
+    public function getErrorMessage(): ?string
+    {
+        return $this->errorMessage;
+    }
+
+    public function getObjectsCount(): int
+    {
+        return $this->objectsCount;
+    }
+}
