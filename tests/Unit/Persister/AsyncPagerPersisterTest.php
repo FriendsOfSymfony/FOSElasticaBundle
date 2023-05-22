5d153381ea352f99d6ea9dab924c8e71c47d0c47
Oleg Andreyev <oleg.andreyev@lampa.lv>
2023-05-22 21:41:23 +0300

    Remove count from async persister (#1905)
    * — insertPage no longer return int, now return void.
    — result from insertPage not used anywhere, and it is resulted in useless and expensive query to database.
    
    * - updated test.
    
    * added annotation for variable.
    
    * phpcs

--- tests/Unit/Persister/AsyncPagerPersisterTest.php
+++ tests/Unit/Persister/AsyncPagerPersisterTest.php
@@ -13,17 +13,12 @@ namespace FOS\ElasticaBundle\Tests\Unit\Persister;
 
 use FOS\ElasticaBundle\Message\AsyncPersistPage;
 use FOS\ElasticaBundle\Persister\AsyncPagerPersister;
-use FOS\ElasticaBundle\Persister\InPlacePagerPersister;
-use FOS\ElasticaBundle\Persister\ObjectPersisterInterface;
 use FOS\ElasticaBundle\Persister\PagerPersisterInterface;
 use FOS\ElasticaBundle\Persister\PagerPersisterRegistry;
-use FOS\ElasticaBundle\Persister\PersisterRegistry;
 use FOS\ElasticaBundle\Provider\PagerInterface;
-use FOS\ElasticaBundle\Provider\PagerProviderInterface;
 use FOS\ElasticaBundle\Provider\PagerProviderRegistry;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\DependencyInjection\ServiceLocator;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\MessageBusInterface;
 
@@ -55,47 +50,5 @@ class AsyncPagerPersisterTest extends TestCase
 
         $pager = $this->createMock(PagerInterface::class);
         $sut->insert($pager);
-    }
-
-    public function testInsertPageReturnObjectCount()
-    {
-        $persistersLocator = $this->createMock(ServiceLocator::class);
-        $persistersLocator->expects($this->once())->method('has')->with('foo')->willReturn(true);
-        $persistersLocator->expects($this->once())->method('get')->with('foo')->willReturn($this->createMock(ObjectPersisterInterface::class));
-
-        $pagerPersistersLocator = $this->createMock(ServiceLocator::class);
-        $pagerPersistersLocator->expects($this->once())->method('has')->with('in_place')->willReturn(true);
-        $pagerPersistersLocator->expects($this->once())->method('get')->with('in_place')->willReturn(
-            new InPlacePagerPersister(
-                new PersisterRegistry($persistersLocator),
-                $this->createMock(EventDispatcherInterface::class)
-            )
-        );
-
-        $pagerPersisterRegistry = new PagerPersisterRegistry($pagerPersistersLocator);
-
-        $pagerMock = $this->createMock(PagerInterface::class);
-        $pagerMock->expects($this->exactly(2))->method('setMaxPerPage')->with(10);
-        $pagerMock->method('setCurrentPage')->withConsecutive([1], [1], [0]);
-        $pagerMock->expects($this->exactly(2))->method('getCurrentPageResults')->willReturn([]);
-
-        $provider = $this->createMock(PagerProviderInterface::class);
-        $provider->expects($this->once())->method('provide')->with([
-            'first_page' => 1,
-            'last_page' => 1,
-            'indexName' => 'foo',
-            'max_per_page' => 10,
-        ])->willReturn($pagerMock);
-
-        $pagerProviderRegistry = $this->createMock(PagerProviderRegistry::class);
-        $pagerProviderRegistry->expects($this->once())->method('getProvider')->with('foo')->willReturn($provider);
-
-        $messageBus = $this->createMock(MessageBusInterface::class);
-        $sut = new AsyncPagerPersister($pagerPersisterRegistry, $pagerProviderRegistry, $messageBus);
-
-        $sut->insertPage(1, [
-            'indexName' => 'foo',
-            'max_per_page' => 10,
-        ]);
     }
 }
